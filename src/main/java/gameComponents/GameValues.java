281554ee365b7232d9aa099c4589f217a1ac020f
Daniel Buis <Daniel.Buis@Milliman.com>
2020-11-01 14:51:51 -0500

    Add framework for bullets
    Still need to do the update and figure out a central location to store a list of bullets

--- src/main/java/gameComponents/GameValues.java
+++ src/main/java/gameComponents/GameValues.java
@@ -4,10 +4,15 @@ public class GameValues {
     //layer values
     public static final int PLAYER_SHIP_LAYER = 4900;
     public static final int BACKGROUND_BASE_LAYER = 5000;
+    public static final int BULLET_LAYER = 4500;
 
     //background values
     public static final int TILES_PER_SIDE = 10;
     public static final int TILE_SIZE = 3;
+
+    //bullet values
+    public static final float BULLET_SPEED = 2.7f;
+    public static final float BULLET_SIZE = 0.1f;
 
     //ship values
     public static final float PLAYER_WIDTH = 1.5f;
@@ -15,4 +20,5 @@ public class GameValues {
     public static final float PLAYER_ACCELERATION = 0.2f;
     public static final float PLAYER_TURN_RATE = 1.5f;
     public static final float PLAYER_MAX_SPEED = 1.0f;
+
 }
