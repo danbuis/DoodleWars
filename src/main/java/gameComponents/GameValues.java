33e4580ab411391371c430decdf88ba5a1f65fdf
Daniel Buis <Daniel.Buis@Milliman.com>
2020-11-04 16:00:21 -0500

    Add basic asteroids

--- src/main/java/gameComponents/GameValues.java
+++ src/main/java/gameComponents/GameValues.java
@@ -5,10 +5,14 @@ public class GameValues {
     public static final int PLAYER_SHIP_LAYER = 4900;
     public static final int BACKGROUND_BASE_LAYER = 5000;
     public static final int BULLET_LAYER = 4500;
+    public static final int ASTEROID_LAYER = 4501;
 
     //background values
     public static final int TILES_PER_SIDE = 10;
     public static final int TILE_SIZE = 3;
+
+    public static final float BOARD_EDGE = TILES_PER_SIDE*TILE_SIZE * 0.95f;
+    public static final float BOARD_EDGE_BUFFER = BOARD_EDGE + 3;
 
     //bullet values
     public static final float BULLET_SPEED = 2.7f;
@@ -20,5 +24,26 @@ public class GameValues {
     public static final float PLAYER_ACCELERATION = 0.2f;
     public static final float PLAYER_TURN_RATE = 1.5f;
     public static final float PLAYER_MAX_SPEED = 1.0f;
+    public static final float PLAYER_BULLET_COOLDOWN = 0.7f;
+    public static final float PLAYER_BULLET_BASE_DAMAGE = 25;
 
+    //asteroid values
+    public static final int INITIAL_ASTEROID_COUNT = 4;
+    public static final float ASTEROID_SIZE_1 = 3.0f;
+    public static final float ASTEROID_SIZE_2 = 1.8f;
+    public static final float ASTEROID_SIZE_3 = 1.0f;
+    public static final float ASTEROID_SIZE_4 = 0.5f;
+    public static final float ASTEROID_HEALTH_1 = 1000;
+    public static final float ASTEROID_HEALTH_2 = 600;
+    public static final float ASTEROID_HEALTH_3 = 360;
+    public static final float ASTEROID_HEALTH_4 = 210;
+    public static final float ASTEROID_DAMAGE_1 = 200;
+    public static final float ASTEROID_DAMAGE_2 = 100;
+    public static final float ASTEROID_DAMAGE_3 = 50;
+    public static final float ASTEROID_DAMAGE_4 = 25;
+    public static final float ASTEROID_START_SPEED_MIN = 0.2f;
+    public static final float ASTEROID_START_SPEED_MAX = 0.4f;
+    public static final float ASTEROID_SPEED_MODIFIER_MIN = 1.2f;
+    public static final float ASTEROID_SPEED_MODIFIER_MAX = 1.7f;
+    public static final float ASTEROID_ROTATION_SPEED_MAX = 1.3f;
 }
---
+++ src/main/java/gameComponents/NotShips/Asteroid.java
@@ -0,0 +1,84 @@
+package gameComponents.NotShips;
+
+import BBDGameLibrary.GameEngine.GameItem2d;
+import BBDGameLibrary.Geometry2d.BBDPoint;
+import BBDGameLibrary.Geometry2d.BBDPolygon;
+import BBDGameLibrary.OpenGL.Mesh;
+import BBDGameLibrary.OpenGL.Texture;
+import engine.Utils;
+import gameComponents.GameValues;
+import org.joml.Matrix4f;
+
+public class Asteroid extends GameItem2d {
+    private final float direction;
+    private float health = 0;
+    private float speed = 0;
+    private float rotation = 0;
+
+    private static final Texture texture = new Texture("assets/images/asteroid.jpg");
+
+    public Asteroid(int size_class, float direction, float speed) {
+        super(Mesh.buildMeshFromPolygon(Asteroid.buildPolygon(size_class), texture), Utils.buildBasicTexturedShaderProgram(), Asteroid.buildPolygon(size_class), GameValues.ASTEROID_LAYER, false);
+        this.direction = direction;
+        switch (size_class){
+            case 1:
+                this.health = GameValues.ASTEROID_HEALTH_1;
+                break;
+            case 2:
+                this.health = GameValues.ASTEROID_HEALTH_2;
+                break;
+            case 3:
+                this.health = GameValues.ASTEROID_HEALTH_3;
+                break;
+            case 4:
+                this.health = GameValues.ASTEROID_HEALTH_4;
+                break;
+        }
+        this.speed = speed;
+        this.rotation = (float) (Math.random() * GameValues.ASTEROID_ROTATION_SPEED_MAX);
+    }
+
+    @Override
+    public void update(float interval){
+        this.translate((float)Math.sin(this.direction) * speed * interval,
+                (float)Math.cos(this.direction) * speed * interval);
+
+        this.rotate(this.rotation * interval);
+    }
+
+    @Override
+    public void setUniforms(Matrix4f projectionMatrix, Matrix4f modelViewMatrix) {
+        this.shader.setUniform("projectionMatrix", projectionMatrix);
+        this.shader.setUniform("modelViewMatrix", modelViewMatrix);
+        this.shader.setUniform("texture_sampler", 0);
+    }
+
+    public static BBDPolygon buildPolygon(int size_class){
+        float width = 0;
+        switch (size_class){
+            case 1:
+                width = GameValues.ASTEROID_SIZE_1;
+                break;
+            case 2:
+                width = GameValues.ASTEROID_SIZE_2;
+                break;
+            case 3:
+                width = GameValues.ASTEROID_SIZE_3;
+                break;
+            case 4:
+                width = GameValues.ASTEROID_SIZE_4;
+                break;
+        }
+        float sqrt = (float) (Math.sqrt(2) * width/2);
+        return new BBDPolygon(new BBDPoint[]{
+                new BBDPoint(width/2, 0),
+                new BBDPoint(sqrt, sqrt),
+                new BBDPoint(0, width/2),
+                new BBDPoint(-sqrt, sqrt),
+                new BBDPoint(-width/2, 0),
+                new BBDPoint(-sqrt, -sqrt),
+                new BBDPoint(0, -width/2),
+                new BBDPoint(sqrt, -sqrt)
+        });
+    }
+}
