bab09f611acec512f4438d94cdfc5991685e2ae5
Daniel Buis <Daniel.Buis@Milliman.com>
2020-11-08 20:06:16 -0500

    Refactor spawn
    Asteroids don't spawn on top of eachother

--- src/main/java/gameComponents/NotShips/Spawner.java
+++ src/main/java/gameComponents/NotShips/Spawner.java
@@ -1,13 +1,45 @@
 package gameComponents.NotShips;
 
+import BBDGameLibrary.GameEngine.GameItem;
 import engine.DoodleWarsGame;
 import gameComponents.GameValues;
+import org.joml.Vector2f;
 
 public class Spawner {
     private float elapsedTime = 0;
     public static final int SPAWN_ASTEROID = 0;
 
     public void spawnItem(int itemToSpawn){
+        switch (itemToSpawn){
+            case SPAWN_ASTEROID:
+                Vector2f coords = this.getSpawnCoords();
+                boolean validSpot = false;
+                int count = 0;
+                while(!validSpot && count<10){
+                    validSpot = true;
+                    count++;
+
+                    for (GameItem asteroid: DoodleWarsGame.asteroidList){
+                        float deltaX = asteroid.getPosition().x - coords.x;
+                        float deltaY = asteroid.getPosition().y - coords.y;
+
+                        float distance = deltaX*deltaX + deltaY*deltaY;
+
+                        if (distance > GameValues.ASTEROID_SIZE_1 * 1.1){
+                            validSpot = false;
+                            coords = this.getSpawnCoords();
+                        }
+                    }
+                }
+                float speed = (float) ((Math.random() * (GameValues.ASTEROID_START_SPEED_MAX - GameValues.ASTEROID_START_SPEED_MIN)) + GameValues.ASTEROID_START_SPEED_MIN);
+                Asteroid asteroid = new Asteroid(1, (float) ((Math.PI * 4 * Math.random()) - Math.PI*2), speed);
+                DoodleWarsGame.asteroidList.add(asteroid);
+                asteroid.setPosition(coords.x, coords.y);
+                System.out.println(coords.x+", "+coords.y);
+        }
+    }
+
+    public Vector2f getSpawnCoords(){
         float lateralValue = GameValues.BOARD_EDGE_BUFFER - (float) (Math.random() * 2 * GameValues.BOARD_EDGE_BUFFER);
         int side = (int) Math.ceil(Math.random() * 4);
 
@@ -36,14 +68,7 @@ public class Spawner {
                 y = 0;
         }
 
-        switch (itemToSpawn){
-            case SPAWN_ASTEROID:
-                float speed = (float) ((Math.random() * (GameValues.ASTEROID_START_SPEED_MAX - GameValues.ASTEROID_START_SPEED_MIN)) + GameValues.ASTEROID_START_SPEED_MIN);
-                Asteroid asteroid = new Asteroid(1, (float) ((Math.PI * 4 * Math.random()) - Math.PI*2), speed);
-                DoodleWarsGame.asteroidList.add(asteroid);
-                asteroid.setPosition(x, y);
-                System.out.println(x+", "+y);
-        }
+        return new Vector2f(x,y);
     }
 
     public void update(float interval){
